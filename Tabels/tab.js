ec9dcbb7c18930d8a88d4497eaa5859abb19634e
Ali-zaib12 <161868911+Ali-zaib12@users.noreply.github.com>
2024-05-19 21:22:36 +0500

    Add files via upload

---
+++ Tabels/tab.js
@@ -0,0 +1,4 @@
+var n1 = 2;
+for (var a = 1; a <= 10; a++) {
+    console.log("".concat(n1, " * ").concat(a, " = ").concat(n1 * a));
+}
---
+++ Tabels/tab.ts
@@ -0,0 +1,5 @@
+let n1 = 2
+
+for( let a:number = 1; a<=10; a++){
+    console.log(`${n1} * ${a} = ${n1*a}`)
+}
